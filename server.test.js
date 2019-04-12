5b8ad3716f012109d289a97b29772f942744d378
workingjubilee <workingjubilee@gmail.com>
2019-04-12 09:12:40 -0700

    Fix test syntax

--- server.test.js
+++ server.test.js
@@ -7,7 +7,7 @@ const server = require('./server.js');
 // - [ ] Your API must be have `POST` and `GET` endpoints for `/games`.
 // - [ ] Write a **minimum** of three tests per endpoint.
 
-- [ ] The `POST /games` endpoint should take in an object that looks like this
+// - [ ] The `POST /games` endpoint should take in an object that looks like this
 
 //   ```js
 //   {
@@ -29,12 +29,12 @@ const server = require('./server.js');
 
 describe('server.js', () => {
 
-  describe('POST', () => {
+  describe('POST /games', () => {
     it('should throw a fit if handed a malformed schema', async() => {
 
       const badSchema = {title: "Dungeons & Dragons: Satan\'s Game"};
 
-      const response = await server.request(badSchema);
+      const response = await request(server).post('/games').send(badSchema);
 
       expect(response.status).toEqual(422);
     })
@@ -43,16 +43,16 @@ describe('server.js', () => {
 
       const goodSchema = {title: "DooM", genre: "First-Person Shooter", releaseYear: "1993" };
 
-      const response = await server.request(goodSchema);
+      const response = await request(server).post('/games').send(goodSchema);
 
       expect(response.status).toEqual(200);
     })
 
     it('should allow releaseYear to be optional', async() => {
 
-      const optionalSchema = {title: "The Dozens", genre: "Insults" };
+      const optionalSchema = {title: "Wrestling", genre: "Contact Sports" };
 
-      const response = await request(server).post('/games/').(optionalSchema);
+      const response = await request(server).post('/games').send(optionalSchema);
 
       expect(response.type).toEqual('application/json');
     });
@@ -61,30 +61,30 @@ describe('server.js', () => {
 
       const optionalSchema = {title: "The Dozens", genre: "Insults" };
 
-      const response = await request(server).post('/games/').(optionalSchema);
+      const response = await request(server).post('/games').send(optionalSchema);
 
       expect(response.type).toEqual('application/json');
     })
   });
 
-  describe('GET', () => {
+  describe('GET /games', () => {
     it('should return a good response on a GET', async() => {
 
-      const response = await request(server).get('/games/');
+      const response = await request(server).get('/games');
 
       expect(response.status).toEqual(200);
     })
 
-    it('should use JSON', async() => {
+    it('should respond with JSON', async() => {
 
-      const response = await request(server).get('/games/');
+      const response = await request(server).get('/games');
 
       expect(response.type).toEqual('application/json');
     })
 
     it('should return the list of games', async() => {
 
-      const response = await request(server).get('/games/');
+      const response = await request(server).get('/games');
 
       expect(response.body).toEqual(expect.any(Array));
     })
